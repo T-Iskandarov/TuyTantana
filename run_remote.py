f474553d0d4a049f4e7ab80d68a367f6a81ee12c
T_Iskandarov <tursunpulatiskandarov@gmail.com>
2026-07-26 21:05:08 +0500

    fix(deploy): preserve uploads and media folders during git clean in run_remote.py

--- run_remote.py
+++ run_remote.py
@@ -14,6 +14,7 @@ client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect('169.58.49.5', username='root', password='Ferrari3377274', timeout=10)
 
 run_cmd(client, 'cd /root/tuy-tantana && rm -f django_backend/.secret_key django_backend/security.log')
+run_cmd(client, 'cd /root/tuy-tantana && git reset --hard && git clean -fd -e uploads/ -e media/ -e django_backend/uploads/ -e venv/')
 run_cmd(client, 'cd /root/tuy-tantana && git pull origin main')
 run_cmd(client, 'cd /root/tuy-tantana/django_backend && source venv/bin/activate && python manage.py makemigrations')
 run_cmd(client, 'cd /root/tuy-tantana/django_backend && source venv/bin/activate && python manage.py migrate')
